9791c9f1e974b7fc3a11d5f7903ba6351a4132bc
Zane Wolf <30910417+zanewolf@users.noreply.github.com>
2020-11-09 14:57:57 -0500

    finished lab 11

--- Week 11/submission_week-10_ZaneWolf/Lab_11/js/main.js
+++ Week 11/submission_week-10_ZaneWolf/Lab_11/js/main.js
@@ -1,5 +1,6 @@
 // Variable for the visualization instance
-let stationMap;
+let stationMap,
+lineData;
 
 
 // Hubway JSON feed
@@ -8,6 +9,12 @@ let url = 'https://member.bluebikes.com/data/stations.json';
 /*d3.json(url).then(jsonData =>{
 	console.log(jsonData);
 });*/
+
+d3.json("data/MBTA-Lines.json")
+    .then(d=>{
+        lineData=d;
+    })
+
 
 fetch(url, function(d){
     console.log(d)
@@ -21,20 +28,51 @@ fetch(url, function(d){
 // From the data, we're creating the final data structure we need and create a new instance of the StationMap
 function gettingStarted(data) {
 
-    // log data
-    console.log(data)
-
     // Extract list with stations from JSON response
 
 
-    // create empty data structure
+    // variations for creating objects with stationname as key or index as key
 
-    // Prepare data by looping over stations and populating empty data structure
+    // data.stations.forEach(d=>console.log(d.s))
+    // let displayData = {};
+
+    // data.stations.forEach(function(d,i){
+
+    //     // station name as main key
+    //     // displayData[d.s]={};
+    //     // displayData[d.s]["bikes"]=d.ba;
+    //     // displayData[d.s]["docks"]=d.da;
+    //     // let lat = d.la;
+    //     // let long = d.lo;
+    //     // displayData[d.s]["coordinates"]=[lat, long];
+
+    //     // index as main key
+    //     displayData[i]={};
+    //     displayData[i]["station"]=d.s;
+    //     displayData[i]["bikes"]=d.ba;
+    //     displayData[i]["docks"]=d.da;
+    //     let lat = d.la;
+    //     let long = d.lo;
+    //     displayData[i]["coordinates"]=[lat, long];
+    //
+    // })
+
+    displayData=[];
+    data.stations.forEach((d,i)=>{
+        displayData.push({
+            index: i,
+            station: d.s,
+            bikes: d.ba,
+            docks: d.da,
+            lat: d.la,
+            long: d.lo,
+        })
+    })
 
 
     // Display number of stations in DOM
-    //$("#station-count")...
+    $("#station-count").text(displayData.length)
 
     // Instantiate visualization object (bike-sharing stations in Boston)
-    stationMap = new StationMap("station-map", displayData, [42.360082, -71.058880]);
+    stationMap = new StationMap("station-map", displayData,[42.360082, -71.058880], lineData);
 }
--- Week 11/submission_week-10_ZaneWolf/Lab_11/js/stationMap.js
+++ Week 11/submission_week-10_ZaneWolf/Lab_11/js/stationMap.js
@@ -10,9 +10,11 @@ class StationMap {
 	/*
 	 *  Constructor method
 	 */
-	constructor(parentElement, displayData) {
+	constructor(parentElement, displayData, mapCenter, lineData) {
 		this.parentElement = parentElement;
 		this.displayData = displayData;
+		this.mapCenter = mapCenter;
+		this.lineData = lineData;
 
 		this.initVis();
 	}
@@ -23,6 +25,17 @@ class StationMap {
 	 */
 	initVis () {
 		let vis = this;
+
+		L.Icon.Default.imagePath = 'images/';
+
+
+		vis.map=L.map(vis.parentElement).setView(vis.mapCenter, 13);
+
+		L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+			attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+		}).addTo(vis.map);
+
+
 
 		vis.wrangleData();
 	}
@@ -43,6 +56,38 @@ class StationMap {
 	updateVis() {
 		let vis = this;
 
+		// see ya, dworky
+		// let dworkin = L.marker([42.378774,-71.117303]).bindPopup("Maxwell Dworkin");
+		// vis.map.addLayer(dworkin);
+
+		// add markers
+		vis.stations = L.layerGroup().addTo(vis.map)
+
+		vis.displayData.forEach((d,i)=>{
+			vis.marker = L.marker([d.lat, d.long]).addTo(vis.map).bindPopup(d.station + "<br> Available Bikes: " + d.bikes + "<br> Available Docks: " + d.docks)
+			vis.stations.addLayer(vis.marker)
+
+		})
+
+		// add MBTA lines
+		let lines = L.geoJson(vis.lineData, {
+			style: styleLines,
+			weight: 5,
+			fillOpacity: 0.7
+		}).addTo(vis.map)
+		// L.geoJson(vis.lineData).addTo(vis.map)
+
+
+		function styleLines(feature){
+			// console.log(feature)
+			switch(feature.properties.LINE){
+				case 'RED': return {color: "red"};
+				case 'GREEN': return {color: "green"};
+				case 'ORANGE': return {color: "orange"};
+				case 'SILVER': return {color: "grey"};
+				case 'BLUE': return {color: "blue"};
+			}
+		}
 
 	}
 }
